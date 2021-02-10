68b2f7f76632171f5df2b8c2005e6addb1b0e64b
Jerry Vinokurov <grapesmoker@gmail.com>
2021-02-10 09:04:21 -0500

    reasonable settings

--- ketl/db/settings.py
+++ ketl/db/settings.py
@@ -1,16 +1,27 @@
 import os
+import configparser
 
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 
-DB_CONN_STRING = os.getenv('DB_DSN', 'sqlite:///ketl.db')
+CONFIG_FILE = Path('ketl.ini')
+
+if CONFIG_FILE.exists():
+    parser = configparser.ConfigParser()
+    parser.read(CONFIG_FILE)
+    if 'ketl' in parser:
+        DB_DSN = parser.get('ketl', 'DB_DSN', fallback=os.getenv('DB_DSN', 'sqlite:///ketl.db'))
+    else:
+        DB_DSN = os.getenv('DB_DSN', 'sqlite:///ketl.db')
+else:
+    DB_DSN = os.getenv('DB_DSN', 'sqlite:///ketl.db')
 
 
-def get_engine():
+def get_engine(conn_string=DB_DSN):
     # extremely primitive memoization to get a single global engine object
-    conn_string = os.getenv('DB_DSN', DB_CONN_STRING)
     return get_session.__dict__.setdefault(f'_engine:{conn_string}', create_engine(conn_string))
 
 
